92dc0bed890a731ef0165043a05c7381f421770b
soumiblink <soumiblink@gmail.com>
2026-03-04 21:22:25 +0530

    Fix import paths from ~/ to @/ in components

--- app/components/ui/stateful-button.tsx
+++ app/components/ui/stateful-button.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { cn } from "~/lib/utils";
+import { cn } from "@/lib/utils";
 import React from "react";
 import { motion, AnimatePresence, useAnimate } from "motion/react";
 
